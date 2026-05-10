a8ea57d16037126a633d2d7e5f70e856faccab05
Bryan <buungoc1805@gmail.com>
2026-05-10 10:11:42 +0000

    feat: wire prisma, auth to server

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,47 +1,28 @@
 import 'dotenv/config';
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
-import {PrismaClient} from '@prisma/client';
-import pg, { Pool } from 'pg';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { request } from 'node:http';
-import { error } from 'node:console';
+import {prisma} from './lib/prisma';
+import {auth_routes} from './routes/auth';
 
-const pool = new pg.Pool({
-    connectionString: process.env.DATABASE_URL,
-});
-
-const adapter = new PrismaPg(pool);
-const prisma = new PrismaClient({adapter});
-
-// Init Fastify server
 const server = Fastify({logger: true});
+server.register(cors, {origin: true});
 
-// Register plugins
-server.register(cors, {origin: true,});
+// Register the modular route files
+server.register(auth_routes);
 
-// Create a check route to test the db
-server.get("/ping", async (request, reply) => {
+server.get('/ping', async (request, reply) => {
     try {
         const player_count = await prisma.player.count();
-
-        return {
-            message: "Astral API is now online.",
-            database: "Connected",
-            total_player: player_count
-        };
-    } catch {error} {
+        return {message: "Astral API is now online", total_players: player_count};
+    } catch (error) {
         server.log.error(error);
         return reply.status(500).send({error: "Database connection failed"});
     }
 });
 
-// Boot the server
 const start = async () => {
     try {
-        // Listen to port 10000
-        // host 0.0.0.0 to ensures it works with Docker and WSL
-        await server.listen({port: 10000, host: '0.0.0.0'});
+        await server.listen({port: 10000, host: "0.0.0.0"});
         console.log("Server is running at http://localhost:10000");
     } catch (err) {
         server.log.error(err);
